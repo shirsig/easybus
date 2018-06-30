be71b6cd756e05966a411d364e8ec87a8e7be82b
Simon Hirsig <simon.hirsig@gmail.com>
2018-06-30 06:53:04 +0200

    easybus metadata

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -38,9 +38,20 @@
            (ANY "*" []
              (route/not-found (slurp (io/resource "404.html")))))
 
+(defn allow-cross-origin
+  "middleware function to allow cross origin"
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (-> response
+        (assoc-in [:headers "Access-Control-Allow-Origin"]  "*")
+        (assoc-in [:headers "Access-Control-Allow-Methods"] "GET,PUT,POST,DELETE,OPTIONS")
+        (assoc-in [:headers "Access-Control-Allow-Headers"] "X-Requested-With,Content-Type,Cache-Control")))))
+
 (def app (-> routes
              (middleware/wrap-json-body {:keywords? true :bigdecimals? true})
-             (middleware/wrap-json-response)))
+             (middleware/wrap-json-response)
+             allow-cross-origin))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
